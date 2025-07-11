9006a7da6f937c9153c728e5a9f275877865a59c
Magnus Lidström <malstrom72@gmail.com>
2025-07-12 00:44:57 +0200

    Organize projects and remove old directories

--- lab/impala/test.cmd
+++
@@ -1,3 +0,0 @@
-../build/Mac/GCC/Debug/AdventGAZL <testInput0.txt | diff testOutput0.txt -
-../build/Mac/GCC/Debug/AdventGAZL <testInput1.txt | diff testOutput1.txt -
-../build/Mac/GCC/Debug/AdventGAZL <testInput2.txt | diff testOutput2.txt -
