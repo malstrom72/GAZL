6d8b7b8fafe9b2fa7eac60a36b94564dd18789c7
Magnus Lidström <malstrom72@gmail.com>
2025-07-17 13:57:32 +0200

    Fix license indentation

--- src/GAZL.h
+++ src/GAZL.h
@@ -36,24 +36,20 @@
 	Version 1.0
 	
 	\page Copyright
-	
-	GAZL is released under the "New Simplified BSD License". http://www.opensource.org/licenses/bsd-license.php
-	
-	Copyright (c) 2010-2012, NuEdge Development / Magnus Lidstroem
-	All rights reserved.
+
+	GAZL is released under the BSD 2-Clause License.
+
+	Copyright (c) 2010-2025, Magnus Lidström
 
 	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 	following conditions are met:
 
 	Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-	disclaimer. 
-	
+	disclaimer.
+
 	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
-	disclaimer in the documentation and/or other materials provided with the distribution. 
-	
-	Neither the name of the NuEdge Development nor the names of its contributors may be used to endorse or promote
-	products derived from this software without specific prior written permission.
-	
+	disclaimer in the documentation and/or other materials provided with the distribution.
+
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
